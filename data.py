373f3277df968520335548771d9b60217a83a3ff
Kassia-Guimaraes <kassiavivereduarda@gmail.com>
2024-04-01 21:06:01 +0100

    Primeiro up

---
+++ data.py
@@ -0,0 +1,146 @@
+import pandas as pd
+
+regioes = {
+    'Aveiro': 'Norte',
+    'Beja': 'Alentejo',
+    'Braga': 'Norte',
+    'Bragança': 'Norte',
+    'Castelo Branco': 'Centro',
+    'Coimbra': 'Centro',
+    'Évora': 'Alentejo',
+    'Faro': 'Algarve',
+    'Guarda': 'Centro',
+    'Leiria': 'Centro',
+    'Lisboa': 'Lisboa',
+    'Portalegre': 'Alentejo',
+    'Porto': 'Norte',
+    'Santarém': 'Centro',
+    'Setúbal': 'Lisboa',
+    'Viana do Castelo': 'Norte',
+    'Vila Real': 'Norte',
+    'Viseu': 'Centro',
+    'Açores':'Açores',
+    'Madeira':'Madeira'
+}
+
+
+### overall2019
+overall2019_df = pd.read_csv('./oldDataSet/legislativas2019/overall_results.csv', sep=',')
+
+overall2019_df[['date', 'time']] = overall2019_df['time'].str.split(' ').to_list()
+overall2019_df = overall2019_df[['date','time', 'territoryFullName', 'territoryName', 'territoryKey', 'totalMandates', 'availableMandates', 'numParishes', 'numParishesApproved', 'blankVotes', 'blankVotesPercentage', 'nullVotes', 'nullVotesPercentage', 'votersPercentage', 'subscribedVoters', 'totalVoters', 'pre.totalMandates', 'pre.availableMandates', 'pre.blankVotes', 'pre.blankVotesPercentage', 'pre.nullVotes', 'pre.nullVotesPercentage', 'pre.votersPercentage', 'pre.subscribedVoters', 'pre.totalVoters']] 
+
+overall2019_df = overall2019_df.fillna(int(0))
+overall2019_df = overall2019_df.drop(overall2019_df.loc[overall2019_df['time'] < '20:10:02'].index)
+overall2019_df = overall2019_df.drop(['territoryFullName','pre.totalMandates', 'pre.availableMandates', 'pre.blankVotes', 'pre.blankVotesPercentage', 'pre.nullVotes', 'pre.nullVotesPercentage', 'pre.votersPercentage', 'pre.subscribedVoters', 'pre.totalVoters'], axis=1) #coluna territoryFullName = territoryName
+
+overall2019_df = overall2019_df.rename(columns={'date':'Data', 'time':'Hora', 'territoryName':'Distrito', 'territoryKey':'Código Territorial', 'totalMandates':'Total Mandatos', 'availableMandates':'Mandatos Disponíveis', 'numParishes':'Total Freguesias', 'numParishesApproved':'Freguesias Apuradas', 'blankVotes':'Votos Brancos', 'blankVotesPercentage':'Percentagem Votos Brancos', 'nullVotes':'Votos Nulos', 'nullVotesPercentage':'Percentagem Votos Nulos', 'votersPercentage':'Percentagem Votantes', 'subscribedVoters':'Votantes Inscritos', 'totalVoters':'Total de Votos'})
+
+overall2019_df['Região'] = overall2019_df['Distrito'].map(regioes)
+
+overall2019_df.to_csv('./modificatedData/overall2019.csv', index=False)
+
+
+
+### parishes2019
+parishes2019_df = pd.read_csv('./oldDataSet/legislativas2019/parishes.csv', sep=',')
+
+parishes2019_df[['date','time']] = parishes2019_df['time'].str.split(' ').to_list()
+parishes2019_df = parishes2019_df[['date','time', 'territoryFullName', 'territoryName', 'territoryKey', 'totalMandates', 'availableMandates', 'numParishes', 'numParishesApproved', 'blankVotes', 'blankVotesPercentage', 'nullVotes', 'nullVotesPercentage', 'votersPercentage', 'subscribedVoters', 'totalVoters', 'pre.totalMandates', 'pre.availableMandates', 'pre.blankVotes', 'pre.blankVotesPercentage', 'pre.nullVotes', 'pre.nullVotesPercentage', 'pre.votersPercentage', 'pre.subscribedVoters', 'pre.totalVoters', 'Council', 'District']]
+
+parishes2019_df = parishes2019_df.fillna(int(0))
+parishes2019_df = parishes2019_df.drop(parishes2019_df[['territoryFullName',"pre.totalMandates","pre.availableMandates","pre.blankVotes","pre.blankVotesPercentage","pre.nullVotes","pre.nullVotesPercentage","pre.votersPercentage","pre.subscribedVoters","pre.totalVoters"]], axis=1) #coluna territoryFullName = territoryName
+parishes2019_df = parishes2019_df.drop(parishes2019_df[["numParishes", "numParishesApproved"]], axis=1)
+
+parishes2019_df = parishes2019_df.rename(columns={'date':'Data','time':'Hora', 'territoryName':'Freguesia', 'territoryKey':'Código Territorial', 'totalMandates':'Total Mandatos', 'availableMandates':'Mandatos Disponíveis', 'blankVotes':'Votos Brancos', 'blankVotesPercentage':'Percentagem Votos Brancos', 'nullVotes':'Votos Nulos', 'nullVotesPercentage':'Percentagem Votos Nulos', 'votersPercentage':'Percentagem Votantes', 'subscribedVoters':'Votantes Inscritos', 'totalVoters':'Total de Votos', 'Council':'Concelho', 'District':'Distrito'})
+
+parishes2019_df['Região'] = parishes2019_df['Distrito'].map(regioes)
+
+parishes2019_df.to_csv('./modificatedData/parishes2019.csv', index=False)
+
+
+
+### result_parishes
+result_parishes2019_df = pd.read_csv('./oldDataSet/legislativas2019/votes_parishes.csv', sep=',')
+
+result_parishes2019_df = result_parishes2019_df.rename(columns={"Trade":'Freguesia',"Party":'Partido',"Percentage":'Percentagem',"validVotesPercentage":'Percentagem de Votos Válidos',"Votes":'Votos',"Council":'Concelho',"District":'Distrito'})
+
+result_parishes2019_df['Região'] = result_parishes2019_df['Distrito'].map(regioes)
+
+result_parishes2019_df.to_csv('./modificatedData/result_parishes2019.csv', index=False)
+
+
+
+# parties
+
+parties = {'Partido': ['ADN','ASDI','B.E.','CDS-PP','CH','IL','L','MDP/CDE','PAN','PCP','PEV','PCP/PEV','PPD/PSD','PPM','PRD','PS','PSN','UDP','UEDS','A', 'R.I.R.','NC','PNR','PURP','PCTP/MRPP','PDR','MPT','JPP','MAS']}
+
+parties_df = pd.DataFrame(parties)
+
+partido_nome = {
+    'ADN': 'Alternativa Democrática Nacional',
+    'ASDI' : 'Ação Social Democrata Independente',
+    'B.E.': 'Bloco de Esquerda',
+    'CDS-PP': 'Centro Democrático Social - Partido Popular',
+    'CH': 'Chega',
+    'IL': 'Iniciativa Liberal',
+    'L':'Livre',
+    'MDP/CDE' : 'Movimento Democrático Português / Comissão Democrática Eleitoral',
+    'PAN': 'Pessoas, Animais Natureza',
+    'PCP': 'Partido Comunista Português',
+    'PEV':'Partido Ecologista Os Verde',
+    'PCP/PEV':' CDU - Coligação Democrática Unitária',
+    'PPD/PSD': 'Partido Popular Democrático / Partido Social Democrático',
+    'PPM':'Partido Popular Monárquico',
+    'PRD':'Partido Renovação Democrática',
+    'PS': 'Partido Socialista',
+    'PSN':'Partido da Solidariedade Nacional',
+    'UDP': 'União Democrática Popular',
+    'UEDS' :'União da Esqueda para a Democracia Socialista',
+    'A' : 'Aliança',
+    'R.I.R.' : 'Reagir Incluir Reciclar',
+    'NC': 'Nós, Cidadãos!',
+    'PNR': 'Partido Nacional Renovador',
+    'PURP': 'Partido Unido dos Reformados e Pensionistas',
+    'PCTP/MRPP':'Partido Comunista dos Trabalhadores Portugueses / Movimento Reorganizativo do Partido do Proletariado',
+    'PDR' : 'Partido Democrático Republicano',
+    'MPT': 'Movimento Partido da Terra',
+    'JPP' : 'Juntos pelo Povo',
+    'MAS':'Movimento Alternativa Socialista'
+}
+partido_cor ={
+    'ADN': '#204E84', #azul
+    'ASDI' : '#2B2B2B', #preto/cinza
+    'B.E.': '#CC1615', #vermelho
+    'CDS-PP': '#127094', #azul claro
+    'CH': '#1F2052', #roxo
+    'IL': '#00AEEF', #azul claro
+    'L': '#840D11', #vermelho
+    'MDP/CDE': '#C10000', #vermelho
+    'PAN': '#3D9177', #azul/verde
+    'PCP': '#B20000', #vermelho
+    'PEV': '#FDDA0B', #amarelo
+    'PCP/PEV':'#4F4F4F', #cinza
+    'PPD/PSD': '#FF6500', #laranja
+    'PPM': '#2A5392', #azul
+    'PRD':'#216B31', #verde
+    'PS': '#E21F26', #vermelho
+    'PSN':'#2AAAF5', #azul
+    'UDP': '#FF0000', #vermelho
+    'UEDS':'#F40000', #vermelho
+    'A' : '#0030A1', #azul escuro
+    'R.I.R.' : '#02959F', #azul /verde
+    'NC': '#E5A845', #amarelo queimado
+    'PNR' : '#182D81', #azul escuro
+    'PURP' : '#008836', #verde
+    'PCTP/MRPP': '#E00C03', #vermelho
+    'PDR': '#204E84', #azul
+    'MPT': '#154026', #verde escuro
+    'JPP' : '#00AB84', #verde/azul
+    'MAS': '#D50000' #vermelho
+}
+
+parties_df['Nome'] = parties_df['Partido'].map(partido_nome)
+parties_df['Cor'] = parties_df['Partido'].map(partido_cor)
+
+parties_df.to_csv('./modificatedData/paties.csv', index=False)
---
+++ functions.py
@@ -0,0 +1,280 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+## overall2019
+overall2019_df = pd.read_csv('./modificatedData/overall2019.csv', sep=',')
+parishes2019_df = pd.read_csv('./modificatedData/parishes2019.csv', sep=',')
+result_parishes2019_df = pd.read_csv('./modificatedData/result_parishes2019.csv', sep=',')
+
+
+def errorCodes(typeError):
+    return f"\33[91mWARNING: {typeError.title()}\33[0;0m\n"
+
+def exitMessange(index_value,typeMessange):
+    return f"\033[93m{index_value} {typeMessange}\033[0m\n"
+
+def nextMessange(index_value, typeMessange):
+    return f"\033[94m{index_value} {typeMessange}\033[0m\n"
+
+def viewDataFrame(df):
+    return print(f"{df.to_markdown(index=False)}\n")
+
+def filtersMenu(toFilter, selectedFilters): #return index in filterMenu
+    if selectedFilters == []:
+        for index, value in enumerate(toFilter):
+            print(f"\33[1m{index+1} \033[0;0m{value}")
+        print(f"\33[1m{index+2}\033[0;0m selecionar todos\n{exitMessange(0, "sair")}")
+    else:
+        for index, value in enumerate(toFilter):
+            print(f"\33[1m{index+1} \033[0;0m{value}")
+        print(f"\33[1m{index+2}\033[0;0m selecionar todos\n{nextMessange(index+3, 'next')}{exitMessange(0, "sair")}")
+    return index
+
+#dataFrame, elements in dataFrame to filter, information messange
+def getUserFiltersLoop(df, toFilter, messange): #return array with all filters to use
+    theSelectedFilters = []
+    while True:
+        index = filtersMenu(toFilter, theSelectedFilters)
+        try:
+            print(f"\33[0;0mFiltros selecionados: {theSelectedFilters} \33[0;0m\nSe quiser alterar a seleção apenas selecionar novamente o item") #show what filters was select
+            input_value = int(input(f"\33[0;0mEscolha um número para selecionar {messange}: \33[0;0m"))
+
+            if input_value != 0 and input_value != index+2 and input_value != index+3: #when 0 abort all, i+2 select all, i+3 finish
+
+                while input_value < 0 or input_value-1 > len(toFilter):
+                    print(errorCodes("Entrada incorreta"))
+                    input_value = int(input(f"\33[0;0mEscolha um número para selecionar {messange}: \33[0;0m")) #request new input
+
+                selection = toFilter[input_value-1] #the selecion is the input_value on index toFilter
+
+                if selection not in theSelectedFilters: #if selection wasnt select after
+                    theSelectedFilters.append(selection)
+                else: #remove the term if was select after
+                    theSelectedFilters.remove(selection)
+                viewDataFrame(df[theSelectedFilters].drop_duplicates())
+
+            elif input_value == index+2: #selected all filters
+                while True:
+                    allfilters_value = int(input(f"\nDeseja selecionar todos os filtros?\n\033[1m1 \033[0;0mSim\n\033[1m2 \033[0;0mNão\nSelecione uma opção: "))
+                    match allfilters_value:
+                        case 1: #confirm selection all terms
+                            theSelectedFilters = toFilter
+                            print(f"\033[0;0mFiltros selecionados: {theSelectedFilters} \033[0;0m\n")
+                            break
+                        case 2: #return to choice the filters
+                            break  # Break out of the loop without changing theSelectedFilters
+                        case _: #error code
+                            print(errorCodes("Entrada incorreta"))
+
+                if allfilters_value == 1: #before confirmation about selection all filters
+                    break
+                else: #new selection filters
+                    print("\n")
+                    continue
+
+            elif input_value == index+3:# to finish
+                while True:
+                    allfilters_value = int(input(f"\nDeseja continuar com estes filtros {theSelectedFilters}?\n\033[1m1 \033[0;0mSim\n\033[1m2 \033[0;0mNão\nSelecione uma opção: "))
+                    match allfilters_value:
+                        case 1: #confirm selection all terms
+                            print(f"\n\033[0;0mFiltros selecionados: {theSelectedFilters} \033[0;0m\n")
+                            break
+                        case 2: #return to choice the filters
+                            break  # Break out of the loop without changing theSelectedFilters
+                        case _: #error code
+                            print(errorCodes("Entrada incorreta"))
+                if allfilters_value == 1: #before confirmation about selection all filters
+                    break
+                else: #new selection filters
+                    print("\n")
+                    continue
+
+            else: # if choice 0, abort all
+                break
+        except: #errorCodes
+            print('\n\n')
+    return theSelectedFilters #return de array with all selected filters
+
+def userFilter(toFilter, messange):
+    theSelectedFilters = []
+    for index, value in enumerate(toFilter):
+        print(f"\33[1m{index+1} \033[0;0m{value}")
+    input_value = int(input(f"\33[0;0mEscolha um número para selecionar {messange}: \33[0;0m"))
+
+    while input_value <1 and input_value > len(toFilter)-1:
+        print(errorCodes("Entrada incorreta"))
+        input_value = int(input(f"\33[0;0mEscolha um número para selecionar {messange}: \33[0;0m\n"))
+    
+    theSelectedFilters = toFilter[input_value-1]
+       
+    return theSelectedFilters #return de array with all selected filters
+
+def locationFilter(df, locationChoice):
+
+    if locationChoice == 'Freguesia':
+        parishes_choices = []
+        while True:
+            try:
+                print(f'\nEscolha o \033[95mDistrito\033[0;0m que pertence a freguesia\n')
+                district = userFilter(df['Distrito'].drop_duplicates().to_list(), 'a freguisia no distrito')
+
+                print(f'\n\nEscolha o \033[95mConcelho\033[0;0m que pertence a freguesia\n')
+                council = userFilter(df[(df['Distrito']==district)]['Concelho'].drop_duplicates().to_list(), 'a freguesia no concelho')
+
+                print(f'\n\nAgora só escolher as \033[95mFreguesias\033[0;0m\n')
+                location = df[(df['Distrito']==district) & (df['Concelho']==council)]
+                parishes = getUserFiltersLoop(location, location['Freguesia'].drop_duplicates().to_list(), 'a freguesia')
+
+                for element in parishes:
+                    parishes_choices.append(element)
+                parishes_choices = list(set(parishes_choices))
+                
+                other_parishes = int(input(f'Freguesias selecionadas {parishes_choices}\nDeseja selecionar mais alguma freguesia?\n1 Sim\n2 Não\nSelecione um número: '))
+                while other_parishes != 1 and other_parishes!=2:
+                    print(errorCodes('Entrada Invalida'))
+                    
+                    other_parishes = int(input(f'Freguesias selecionadas {parishes_choices}\nDeseja selecionar mais alguma freguesia?\n1 Sim\n2 Não\nSelecione um número: '))
+                
+                if other_parishes == 2:
+                    return parishes_choices                
+            
+            except:
+                print(errorCodes('Entrada Incorreta'))
+
+    elif locationChoice == 'Concelho':
+        council_choices = []
+        while True:
+
+            try:
+                print(f'\nEscolha o \033[95mDistrito\033[0;0m que pertence o concelho\n')
+                district = userFilter(df['Distrito'].drop_duplicates().to_list(), 'a freguisia no distrito')
+
+                print(f'\n\nAgora só escolher os \033[95mConcelhos\033[0;0m\n')
+                location = df[(df['Distrito']==district)]
+                council = getUserFiltersLoop(location, location['Concelho'].drop_duplicates().to_list(), 'o concelho')
+
+                for element in council:
+                    council_choices.append(element)
+                council_choices = list(set(parishes_choices))
+                
+                other_council = int(input(f'Freguesias selecionadas {council_choices}\nDeseja selecionar mais alguma freguesia?\n1 Sim\n2 Não\nSelecione um número: '))
+                while other_council != 1 and other_council !=2:
+                    print(errorCodes('Entrada Invalida'))
+                    
+                    other_parishes = int(input(f'Freguesias selecionadas {council_choices}\nDeseja selecionar mais alguma freguesia?\n1 Sim\n2 Não\nSelecione um número: '))
+                
+                if other_council == 2:
+                    return council_choices                
+            
+            except:
+                print(errorCodes('Entrada Incorreta'))
+
+    elif locationChoice == 'Distrito':
+        print(f'\nEscolha os \033[95mDistrito\033[0;0m\n')
+        district_choices = getUserFiltersLoop(df, df['Distrito'].drop_duplicates().to_list(), 'o distrito')
+                
+        return district_choices                
+
+    else:
+        print(f'\nEscolha as \033[95mRegiões\033[0;0m\n')
+        region_choices = getUserFiltersLoop(df, df['Região'].drop_duplicates().to_list(), 'a região')
+        return region_choices
+
+#axis categorical variable, axis Y numeric variable
+def barChart(df, axisX, axisY):
+
+    if 'data' in df.columns.to_list(): #if the dataframe has a column called 'data'
+        df['data'] = pd.to_datetime(df['data'])
+        
+        df['day'] = df['date'].dt.to_period('D')
+        df['month'] = df['date'].dt.to_period('M')
+        df['year'] = df['date'].dt.to_period('Y')
+
+    if 'Distrito' in df.columns.to_list() and 'Território Nacional' in df['Distrito'].to_list(): #if the dataFrame has a collumns called 'territoryFullName' and if this column has 'National Territory'
+        df = df.drop(df[df['Distrito'] == 'Território Nacional'].index)
+
+    print(f'AxisX {df[axisX]}\nAxisY {df[axisY]}')
+    plt.figure(figsize=(10, 6))
+    plt.bar(df[axisX], df[axisY])
+    plt.title(f'Número de {axisY} por {axisX}')
+    plt.xlabel(axisX)
+    plt.ylabel(axisY)
+
+    plt.xticks(rotation=70)  # Rotaciona os rótulos do eixo x para facilitar a leitura
+    # Formatando a escala do eixo y para não usar notação científica
+    plt.ticklabel_format(style='plain', axis='y')
+    plt.grid(axis='y')  # Adiciona grades apenas no eixo y
+    plt.tight_layout()  # Ajusta automaticamente os espaços para evitar sobreposição de elementos
+    plt.show()
+
+def barGraphic(df, columnX, axisX, axisY):
+
+    if 'data' in df.columns.to_list(): #if the dataframe has a column called 'data'
+        df['data'] = pd.to_datetime(df['data'])
+        
+        df['day'] = df['date'].dt.to_period('D')
+        df['month'] = df['date'].dt.to_period('M')
+        df['year'] = df['date'].dt.to_period('Y')
+
+    toX = pd.DataFrame()
+    for element in axisX:
+        toX = pd.concat([toX, (df[df[columnX]==element])], axis=0)
+    
+    plt.figure(figsize=(10, 6))
+    plt.bar(toX[columnX], toX[axisY])
+    plt.title(f'Número de {axisY} por {columnX}')
+    plt.xlabel(columnX)
+    plt.ylabel(axisY)
+
+    plt.xticks(rotation=70)  # Rotaciona os rótulos do eixo x para facilitar a leitura
+    # Formatando a escala do eixo y para não usar notação científica
+    plt.ticklabel_format(style='plain', axis='y')
+    plt.grid(axis='y')  # Adiciona grades apenas no eixo y
+    plt.tight_layout()  # Ajusta automaticamente os espaços para evitar sobreposição de elementos
+    plt.show()
+
+
+def funcionalidade1(df): 
+    parties = getUserFiltersLoop(result_parishes2019_df, result_parishes2019_df['Partido'].drop_duplicates(), 'os partidos') # list with selected parties
+    print(f'\nAgora que já escolheu os partidos basta escolher a localidade que deseja visualizar\n')
+
+    place = userFilter(['Freguesia', 'Concelho', 'Distrito', 'Região'], 'a localidade')
+    locations = locationFilter(df, place) # list with selected localities
+
+    location_df = pd.DataFrame()
+    for locality, party in zip(locations, parties):
+        location_df = pd.concat([location_df, df[(df[place]==locality) & (df['Partido']==party)]], axis=0) #filtered dataFrame
+    
+    plt.figure(figsize=(10, 6))
+
+    width = 0.2
+    pos = np.arange(len(locations))
+
+    for i, party in enumerate(parties):
+        data_party = location_df[location_df['Partido']==party]
+        pos = [j + i * width for j in range(len(locations))]
+        pos = list(map(lambda x: [x], pos))
+        pos = [p[0] for p in pos]
+        plt.bar(pos, data_party['Votos'], width, label=party)
+
+    plt.xlabel('Localidades')
+    plt.ylabel('Votos')
+    plt.title('Votos por Partido e Local')
+    plt.xticks([p + width / 2 for p in range(len(locations))], locations)
+    plt.legend()
+    plt.tight_layout()
+
+    # Exibir o gráfico
+    plt.show()
+
+    return location_df
+
+funcionalidade1(result_parishes2019_df)
+
+#barGraphic(result_parishes2019_df, 'Partido', ['PS', 'CH'], 'Votos')
+
+#barChart(overall2019_df, axisX='Distrito', axisY='Votos Nulos')
+#user_filters = getUserFiltersLoop(overall2019_df, overall2019_df.columns.tolist())
+    
